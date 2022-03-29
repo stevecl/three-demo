f90441c9b6f376ce97a52608cb19c5e1cb349042
cc <1040457762@qq.com>
2022-03-29 22:23:48 +0800

    update

--- src/views/ar.vue
+++ src/views/ar.vue
@@ -1,45 +1,67 @@
 <template>
   <!-- <div> ar page</div> -->
+  <video id="video" autoplay controls>
+    <source src="/sintel.ogv" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+    <!-- <source src="./1086x716.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'> -->
+  </video>
   <canvas id="app-canvas"></canvas>
 </template>
 
 <script setup>
 import  * as THREE  from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
-import { createAr } from '../libs/tools'
-// console.log('sss', THREE, GLTFLoader)
-let loader = new GLTFLoader()
-console.log('sss',THREE, createAr, loader)
-// var dracoLoader = new DRACOLoader();
-// dracoLoader.setDecoderPath( '/examples/js/libs/draco/' );
-// loader.setDRACOLoader( dracoLoader );
+import { createModel } from './init'
+import { onMounted } from 'vue'
 
-loader.load('/portal.glb', 
-  function (gltf) {
-    let model = gltf.scene
-    model.scale.set(1, .5, 1)
-    // model.position.x = 0
-    // model.position.y = 0
+onMounted(() => init())
 
-    let canvas = document.querySelector('#app-canvas')
-    let app = new createAr(canvas, model, gltf.animations)
-    app.mixer.play('CatWalk')
-  },
-  undefined,
-  function (error) {
-    console.error(error)
-  }
-)
+const init = async () => {
+  let canvas = document.querySelector('#app-canvas')
+  let app = new createModel(canvas, getBox())
+  let door = await getDoor()
+  app.addModel(door)
+}
+
+const getBox = () => {
+  //添加立方体
+  var geometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
+  
+  var video = document.querySelector("#video");
+  var texture = new THREE.VideoTexture(video);
+  texture.wrapS = texture.wrapT = THREE.ClampToEdgeWrapping;
+  texture.minFilter = THREE.LinearFilter;
+  let material = new THREE.MeshBasicMaterial( { map: texture } );
+  return new THREE.Mesh(geometry, material)
+}
+
+const getDoor = () => {
+  return new Promise(resolve => {
+    let loader = new GLTFLoader()
+    loader.load('/portal.glb', 
+      function (gltf) {
+        let model = gltf.scene
+        model.scale.set(1, .5, 1)
+        resolve(model)
+      },
+      undefined,
+      function (error) {
+        console.error(error)
+      }
+    )
+  })
+}
+
 </script>
 
-<script>
-
-</script>
 
 <style lang="less" scoped>
-#app-canvas {
-  width: 500px;
-  height: 500px;
+canvas {
+  display: block;
+}
+
+#video{
+  position: fixed;
+  left:0;
+  bottom:0;
 }
 </style>
---
+++ src/views/init.js
@@ -0,0 +1,93 @@
+import  * as THREE  from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+export class createModel {
+  constructor(canvas, model) {
+    this.scene = createModel.initScene()
+        .add(model)
+        .add(createModel.initAmbientLight())
+        .add(createModel.initDirectionlLight())
+    this.camera = createModel.initCamera()
+    this.renderer = createModel.initRenderer(canvas)
+    this.controls = createModel.initControls(this.camera, this.renderer)
+    this.update(model)
+  }
+
+  static initScene () {
+    let scene = new THREE.Scene()
+    var helper = new THREE.AxesHelper(50);
+    scene.add(helper);
+    // scene.background = new THREE.Color(0x336495)
+    return scene
+  }
+
+  static initAmbientLight () {
+    return new THREE.AmbientLight(0x444444, 1)
+  }
+
+  static initDirectionlLight () {
+    let light = new THREE.DirectionalLight(0xffffff, 2)
+    light.position.set(0, 20, 20)
+    light.castShadow = true;
+    light.shadow.camera.top = 10;
+    light.shadow.camera.bottom = -10;
+    light.shadow.camera.left = -10;
+    light.shadow.camera.right = 10;
+
+    //告诉平行光需要开启阴影投射
+    light.castShadow = true;
+    return light
+  }
+
+  static initCamera () {
+    let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 1000)
+    camera.position.set(0, 0, 2)
+    return camera
+  }
+
+  static initRenderer (canvas) {
+    let renderer = new THREE.WebGLRenderer({ canvas })
+    // renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0xeeeeee);
+    renderer.shadowMap.enabled = true;
+    return renderer
+  }
+
+  static initControls(camera, renderer) {
+    let controls = new OrbitControls(camera, renderer.domElement);
+    //设置控制器的中心点
+    //controls.target.set( 0, 5, 0 );
+    // 如果使用animate方法时，将此函数删除
+    //controls.addEventListener( 'change', render );
+    // 使动画循环使用时阻尼或自转 意思是否有惯性
+    controls.enableDamping = true;
+    //controls.dampingFactor = 0.25;  //动态阻尼系数 就是鼠标拖拽旋转灵敏度
+    controls.enableZoom = true;
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 0.5;
+    controls.minDistance = 1;
+    controls.maxDistance = 2000;
+    controls.enablePan = true;
+    return controls
+  }
+
+  addModel (model) {
+    this.scene.add(model)
+    this.update()
+  }
+
+  update () {
+    this.resize()
+    this.renderer.render(this.scene, this.camera)
+    this.controls.update()
+    requestAnimationFrame(() => this.update())
+  }
+
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+}
